cfacaad1eb270f48cd547b55143de7f11326efb5
Siddhesh Tambe <xabc31101@gmail.com>
2026-03-01 12:40:50 +0530

    edit footer

--- pages/1_About.py
+++ pages/1_About.py
@@ -105,6 +105,6 @@ st.markdown("""
 st.markdown("""
 <hr>
 <div style="text-align:center; color:gray;">
-© 2026 Data Xtraction & Analysis System | TYBSc Computer Science Project
+© 2026 Data Xtraction | TYBSc Computer Science Project
 </div>
 """, unsafe_allow_html=True)
